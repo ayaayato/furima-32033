69ce9329fc62ca8c3d5c7e2eb001e6590aa31c8c
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-11-15 15:04:24 +0900

    バリデーション正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,33 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  validates :name,       presence: true
- validates :first_name,    presence: true
- validates :last_name, presence: true
- validates :first_kana,   presence: true
- validates :last_kana,   presence: true
+
+ validates :first_name,    presence: true,
+            format: {with: /\A[\p{Hiragana}||\p{Katakana}||[ー－]||[一-龠々]]+\z/,
+            #messege: "can only use Full-width japanese"
+            }
+
+ validates :last_name, presence: true,
+            format: {with: /\A[\p{Hiragana}||\p{Katakana}||[ー－]||[一-龠々]]+\z/,
+            #messege: "can only use Full-width japanese"
+            }
+
+ validates :first_kana,   presence: true,
+            format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+            #message: "can only use Full-width kana"
+            }
+
+ validates :last_kana,   presence: true,
+            format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+            #message: "can only use Full-width kana"
+            }
+
  validates :birthday,   presence: true
+
+ validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, 
+            #messege: "requires the use of both numbers and alphabets"
+            }
 
  #has_many :items
  #has_many :boughts
