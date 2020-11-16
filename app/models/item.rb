3c992b0bd03e5388223da53af78a73592d070a49
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-11-16 13:17:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,32 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :sebd_area
+  belongs_to :send_area
   belongs_to :send_fee
   belongs_to :send_day
   has_one_attached :image
+  belongs_to :user
+  #has_one :bought
 
-  validates :genre_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :category
+    validates :status
+    validates :send_area
+    validates :send_fee
+    validates :send_day
+    validates :image
+    validates :title
+    validates :explan
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category
+    validates :status
+    validates :send_area
+    validates :send_fee
+    validates :send_day
+  end
+
+
 end
