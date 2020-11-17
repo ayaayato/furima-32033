d4e5befba3330823e095bff4156204e22de7bb8c
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-11-17 17:09:10 +0900

    コントローラー、ビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :create]
+  before_action :exile_to_index, only: [:edit]
 
   def index 
     @items = Item.all.order("created_at DESC")
@@ -21,6 +22,20 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  if @item.save
+     redirect_to item_path(@item.id)
+  else
+   render :edit
+  end
+  end
   
 
   private
@@ -28,4 +43,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:title, :explan, :image, :price, :category_id, :send_area_id, :status_id, :send_fee_id, :send_day_id ).merge(user_id: current_user.id)
   end
+  
+  def exile_to_index
+    @item = Item.find_by(id:params[:id])
+     unless @item.user_id == current_user.id
+       redirect_to action: :index
+     end
+  end
+  
 end
